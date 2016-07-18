22b70937f03751c98cb0526a9cdca5977e51a5ea
Ivan Stanojevic <ivan@byteout.com>
2016-07-18 11:29:07 +0200

    no message

--- javascript/AzimuthView.js
+++ javascript/AzimuthView.js
@@ -1,89 +1,100 @@
-var owner = document.currentScript.ownerDocument;
+(() => {
 
-class AzimuthView extends BaseView {
+    var owner = document.currentScript.ownerDocument;
 
-    createdCallback() {
-        super.createdCallback();
-        this.azimuth = this.validateAngle(this.getAttribute("azimuth"));
-        this.radius = this.getAttribute("radius") ? parseInt(this.getAttribute("radius")) : 100;
-        this.title = this.getAttribute("title") ? parseInt(this.getAttribute("title")) : 'Azimuth';
-        this.root = this.createShadowRoot();
-        this.innerScale = 0.8;
-        this.template = owner.querySelector("template");
-        this.clone = this.template.content.cloneNode(this.template);
-        this.arrow = $(this.clone).find(".arrow");
-        this.label = $(this.clone).find('.main-label');
-        this.titleLabel = $(this.clone).find('.title-label');
-        this.unitLabel = $(this.clone).find('.unit-label');
-        var discOuter = $(this.clone).find(".disc-outer"),
-            discInner = $(this.clone).find(".disc-inner"),
-            labelN = $(this.clone).find(".label-n"),
-            labelS = $(this.clone).find(".label-s"),
-            labelE = $(this.clone).find(".label-e"),
-            labelW = $(this.clone).find(".label-w"),
-            arrowHeight = this.radius * 0.1,
-            arrowWidth = arrowHeight + 5;
+    class AzimuthView extends BaseView {
 
-        this.root.appendChild(this.clone);
-
-        var labelWidth = labelN.width(),
-            labelHeight = labelN.height();
-
-        labelN
-            .css("top", 0)
-            .css("left", Math.floor(this.radius - (labelWidth / 2)) + "px");
-        labelS
-            .css("bottom", -4)
-            .css("left", Math.floor(this.radius - (labelWidth / 2) + 2) + "px");
-        labelE
-            .css("top", Math.floor(this.radius - (labelHeight / 2)) + "px")
-            .css("right", 4);
-        labelW
-            .css("top", Math.floor(this.radius - (labelHeight / 2)) + "px")
-            .css("left", 2 + 1);
-
-        discOuter
-            .width(this.radius * 2)
-            .height(this.radius * 2)
-            .css("background-color", this.colorPrimary);
-
-        var diff = (this.radius) - (this.radius * this.innerScale);
-        discInner
-            .width(this.radius * 2 * this.innerScale)
-            .height(this.radius * 2 * this.innerScale)
-            .css("top", diff + "px")
-            .css("left", diff + "px")
-            .css("background-color", this.colorSecondary);
-
-        this.arrow
-            .css("border-width", "0px " + (arrowWidth / 2) + "px " + arrowHeight + "px " + (arrowWidth / 2) + "px")
-            .css("border-color", "transparent transparent #fff transparent")
-            .css("left", this.radius - (arrowWidth / 2))
-            .css("top", arrowHeight * 2 + 2)
-            .css("transform-origin", ((arrowWidth / 2)) + "px " + (this.radius - diff - 2) + "px")
-            .css("transform", "rotate(" + this.azimuth + "deg)")
-
-        this.label
-            .css('top', this.radius - (this.label.height() / 2))
-            .css('left', 5)
-            .html(this.azimuth + '°');
-
-        this.titleLabel
-            .css('bottom', this.radius + (this.label.height() / 2) * 1.5)
-            .html(this.title);
-
-        this.unitLabel
-            .css('top', this.radius + (this.label.height() / 2) * 1.5)
-    }
-
-    attributeChangedCallback(attrName, oldVal, newVal) {
-        if (attrName == "azimuth") {
+        createdCallback() {
+            super.createdCallback();
             this.azimuth = this.validateAngle(this.getAttribute("azimuth"));
-            this.arrow.css("transform", "rotate(" + this.azimuth + "deg)");
-            this.label.html(this.azimuth + '°');
+            this.radius = this.getAttribute("radius") ? parseInt(this.getAttribute("radius")) : 100;
+            this.title = this.getAttribute("title") ? parseInt(this.getAttribute("title")) : 'Azimuth';
+            this.root = this.createShadowRoot();
+            this.innerScale = 0.8;
+            this.template = owner.querySelector("template");
+            this.clone = this.template.content.cloneNode(this.template);
+            this.arrow = $(this.clone).find(".arrow");
+            this.label = $(this.clone).find('.main-label');
+            this.titleLabel = $(this.clone).find('.title-label');
+            this.unitLabel = $(this.clone).find('.unit-label');
+            var discOuter = $(this.clone).find(".disc-outer"),
+                discInner = $(this.clone).find(".disc-inner"),
+                labelN = $(this.clone).find(".label-n"),
+                labelS = $(this.clone).find(".label-s"),
+                labelE = $(this.clone).find(".label-e"),
+                labelW = $(this.clone).find(".label-w"),
+                arrowHeight = this.radius * 0.1,
+                arrowWidth = arrowHeight + 5;
+
+            this.root.appendChild(this.clone);
+
+            var labelWidth = labelN.width(),
+                labelHeight = labelN.height();
+
+            labelN
+                .css("top", 0)
+                .css('font-size', this.radius * 0.16)
+                .css("left", Math.floor(this.radius - (labelWidth / 2)) + "px");
+            labelS
+                .css("bottom", - this.radius * 0.04)
+                .css('font-size', this.radius * 0.16)
+                .css("left", Math.floor(this.radius - (labelWidth / 2) + 2) + "px");
+            labelE
+                .css("top", Math.floor(this.radius - (labelHeight / 2)) + "px")
+                .css('font-size', this.radius * 0.16)
+                .css("right", 4);
+            labelW
+                .css("top", Math.floor(this.radius - (labelHeight / 2)) + "px")
+                .css('font-size', this.radius * 0.16)
+                .css("left", 2 + 1);
+
+            discOuter
+                .width(this.radius * 2)
+                .height(this.radius * 2)
+                .css("background-color", this.colorPrimary);
+
+            var diff = (this.radius) - (this.radius * this.innerScale);
+            discInner
+                .width(this.radius * 2 * this.innerScale)
+                .height(this.radius * 2 * this.innerScale)
+                .css("top", diff + "px")
+                .css("left", diff + "px")
+                .css("background-color", this.colorSecondary);
+
+            this.arrow
+                .css("border-width", "0px " + (arrowWidth / 2) + "px " + arrowHeight + "px " + (arrowWidth / 2) + "px")
+                .css("border-color", "transparent transparent #fff transparent")
+                .css("left", this.radius - (arrowWidth / 2))
+                .css("top", arrowHeight * 2 + 2)
+                .css("transform-origin", ((arrowWidth / 2)) + "px " + (this.radius - diff - 2) + "px")
+                .css("transform", "rotate(" + this.azimuth + "deg)");
+
+            this.label
+                .css('top', this.radius - (this.label.height() / 2))
+                .css('left', 5)
+                .css('font-size', this.radius * 0.25)
+                .html(this.azimuth + '°');
+
+            this.titleLabel
+                .css('bottom', this.radius + (this.label.height() / 2) * 1.5)
+                .css('font-size', this.radius * 0.16)
+                .html(this.title);
+
+            this.unitLabel
+                .css('top', this.radius + (this.label.height() / 2) * 1.5)
+                .css('font-size', this.radius * 0.16);
         }
+
+        attributeChangedCallback(attrName, oldVal, newVal) {
+            if (attrName == "azimuth") {
+                this.azimuth = this.validateAngle(this.getAttribute("azimuth"));
+                this.arrow.css("transform", "rotate(" + this.azimuth + "deg)");
+                this.label.html(this.azimuth + '°');
+            }
+        }
+
     }
 
-}
+    document.registerElement("azimuth-view", AzimuthView);
 
-document.registerElement("azimuth-view", AzimuthView);
+})();
--- javascript/ElevationView.js
+++ javascript/ElevationView.js
@@ -1,59 +1,68 @@
-var owner = document.currentScript.ownerDocument;
+(() => {
 
-class ElevationView extends BaseView {
+    var owner = document.currentScript.ownerDocument;
 
-    createdCallback() {
-        super.createdCallback();
+    class ElevationView extends BaseView {
 
-        this.elevation = this.validateAngle(this.getAttribute("elevation"));
-        this.radius = this.getAttribute("radius") ? parseInt(this.getAttribute("radius")) : 100;
-        this.title = this.getAttribute("title") ? parseInt(this.getAttribute("title")) : 'Elevation';
-        this.root = this.createShadowRoot();
-        this.innerScale = 0.8;
-        this.template = owner.querySelector("template");
-        this.clone = this.template.content.cloneNode(this.template);
-        this.arrow = $(this.clone).find(".arrow");
-        this.label = $(this.clone).find('.main-label');
-        this.titleLabel = $(this.clone).find('.title-label');
-        this.unitLabel = $(this.clone).find('.unit-label');
-        var discOuter = $(this.clone).find(".disc-outer"),
-            discInner = $(this.clone).find(".disc-inner");
+        createdCallback() {
+            super.createdCallback();
 
-        this.root.appendChild(this.clone);
-
-        discOuter
-            .width(this.radius * 2)
-            .height(this.radius * 2)
-            .css("background-color", this.colorPrimary);
-
-        var diff = (this.radius) - (this.radius * this.innerScale);
-        discInner
-            .width(this.radius * 2 * this.innerScale)
-            .height(this.radius * 2 * this.innerScale)
-            .css("top", diff + "px")
-            .css("left", diff + "px")
-            .css("background-color", this.colorSecondary);
-
-        this.label
-            .css('top', this.radius - (this.label.height() / 2))
-            .css('left', 5)
-            .html(this.elevation + '°');
-
-        this.titleLabel
-            .css('bottom', this.radius + (this.label.height() / 2) * 1.5)
-            .html(this.title);
-
-        this.unitLabel
-            .css('top', this.radius + (this.label.height() / 2) * 1.5)
-    }
-
-    attributeChangedCallback(attrName, oldVal, newVal) {
-        if (attrName == "elevation") {
             this.elevation = this.validateAngle(this.getAttribute("elevation"));
-            this.label.html(this.elevation + '°');
+            this.radius = this.getAttribute("radius") ? parseInt(this.getAttribute("radius")) : 100;
+            this.title = this.getAttribute("title") ? parseInt(this.getAttribute("title")) : 'Elevation';
+            this.root = this.createShadowRoot();
+            this.innerScale = 0.8;
+            this.template = owner.querySelector("template");
+            this.clone = this.template.content.cloneNode(this.template);
+            this.arrow = $(this.clone).find(".arrow");
+            this.label = $(this.clone).find('.main-label');
+            this.titleLabel = $(this.clone).find('.title-label');
+            this.unitLabel = $(this.clone).find('.unit-label');
+            var discOuter = $(this.clone).find(".disc-outer"),
+                discInner = $(this.clone).find(".disc-inner");
+
+            this.root.appendChild(this.clone);
+
+            discOuter
+                .width(this.radius * 2)
+                .height(this.radius * 2)
+                .css("background-color", this.colorPrimary);
+
+            var diff = (this.radius) - (this.radius * this.innerScale);
+            discInner
+                .width(this.radius * 2 * this.innerScale)
+                .height(this.radius * 2 * this.innerScale)
+                .css("top", diff + "px")
+                .css("left", diff + "px")
+                .css("background-color", this.colorSecondary);
+
+            this.label
+                .css('top', this.radius - (this.label.height() / 2))
+                .css('font-size', this.radius * 0.25)
+                .css('left', 5)
+                .html(this.elevation + '°');
+
+            this.titleLabel
+                .css('bottom', this.radius + (this.label.height() / 2) * 1.5)
+                .css('font-size', this.radius * 0.16)
+                .html(this.title);
+
+            this.unitLabel
+                .css('font-size', this.radius * 0.16)
+                .css('top', this.radius + (this.label.height() / 2) * 1.5)
         }
+
+        attributeChangedCallback(attrName, oldVal, newVal) {
+            if (attrName == "elevation") {
+                this.elevation = this.validateAngle(this.getAttribute("elevation"));
+                this.label.html(this.elevation + '°');
+            }
+        }
+
     }
 
-}
+    document.registerElement("elevation-view", ElevationView);
 
-document.registerElement("elevation-view", ElevationView);
+})();
+
+
